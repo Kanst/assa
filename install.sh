e8af0bfd71c7ec53b19f0e512c374a7ba1ebd4e5
kanst9 <kanst9@yandex-team.ru>
2015-02-17 14:39:10 +0300

    assa

--- install.sh
+++ install.sh
@@ -7,16 +7,23 @@
 ###
 
 zypper in postgresql postgresql-server postgresql-contrib
-zypper in vsftpd
+zypper in vsftpd git-core
 
 # vsftpd
+git clone https://github.com/Kanst/assa.git /home/assa/
 useradd assa
 passwd assa
-mkdir /home/assa ; chown -R assa /home/assa
+chown -R assa /home/assa
 sed -i 's/#seccomp_sandbox=NO/seccomp_sandbox=NO/g' /etc/vsftpd.conf 
 sed -i '/write_enable=/s/NO/YES/' /etc/vsftpd.conf 
+systemctl restart vsftpd.service
+
 
 # Создание базы и пользователя (работаем от пользователя postgres)
+sed -i  '/listen_addresses = /s/localhost/*/' /var/lib/pgsql/data/postgresql.conf  
+sed -i 's/#log_duration = off/log_duration = on/g' /var/lib/pgsql/data/postgresql.conf
+sed -i 's/#log_min_duration_statement = -1/log_min_duration_statement = 0/g' /var/lib/pgsql/data/postgresql.conf
+
 createuser assa
 createdb -O assa assa
 psql assa < 1_\(Cre_main\).SQL
